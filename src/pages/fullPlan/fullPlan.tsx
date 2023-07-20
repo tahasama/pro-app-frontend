ea172842edadc726bf5ca0f410b019984fbde709
tahasama <taha.maatof@gmail.com>
2023-07-20 23:18:52 +0100

    resolved clickable plan areas / almost done

--- src/pages/fullPlan/fullPlan.tsx
+++ src/pages/fullPlan/fullPlan.tsx
@@ -1,31 +1,48 @@
+import plan from "../../images/plan.png";
 import { Link } from "react-router-dom";
+
 import "./fullPlan.css";
 import NavBar from "../Navbar/navbar";
-import plan from "../../images/plan.png";
-import Box from "@mui/material/Box";
+import { locations } from "../../constants/constant";
+import { Box } from "@mui/material";
 
 const FullPlan = () => {
-  // Replace with the actual dimensions of your image
-  const YOUR_IMAGE_WIDTH = 4065;
-  const YOUR_IMAGE_HEIGHT = 2281;
-
-  // Replace with your desired percentage values
-  const desiredTopPercentage = (500 / YOUR_IMAGE_HEIGHT) * 100;
-  const desiredLeftPercentage = (500 / YOUR_IMAGE_WIDTH) * 100;
-
   return (
     <div className="fullPage">
       <div className="navbar">
         <NavBar />{" "}
       </div>
 
-      <Box className="masterPlan">
-        <img src={plan} alt="" className="masterPlan" />
-        <div key="secondaryClarifierP24">
-          <div className="secondaryClarifierP24">
-            {/* Your secondaryClarifierP24 content */}
+      <Box
+        className="masterPlan"
+        sx={{
+          mt: { sx: 0, sm: 8, md: 6, lg: -1 },
+          transform: {
+            sx: "scale(.9)",
+            sm: "scale(1)",
+            md: "scale(1)",
+            lg: "scale(.85)",
+          },
+        }}
+      >
+        <img
+          src={plan}
+          alt=""
+          className="masterPlan"
+          style={{ transform: "scale(.9)" }}
+        />
+        {locations.map((i: any) => (
+          <div key={i}>
+            <Link to={`../${i}`} className={i}></Link>
+            <div className={"name-" + i}>
+              <i>
+                <b>
+                  <u>{i}</u>
+                </b>
+              </i>
+            </div>
           </div>
-        </div>
+        ))}
       </Box>
     </div>
   );
